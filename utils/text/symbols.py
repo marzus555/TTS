d1c0d0bc26a57cb1a553b4050b5dd07f6149b57e
marzus555 <62560070+marzus555@users.noreply.github.com>
2020-04-18 13:24:42 +0300

    Update symbols.py

--- utils/text/symbols.py
+++ utils/text/symbols.py
@@ -5,7 +5,7 @@ Defines the set of symbols used in text input to the model.
 The default is a set of ASCII characters that works well for English or text that has been run
 through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details.
 '''
-from utils.text.en_symbols import _pad, _eos, _bos, _characters, _punctuations, _phoneme_punctuations, _phonemes, _arpabet, phonemes, symbols
+from utils.text.ro_symbols import _pad, _eos, _bos, _characters, _punctuations, _phoneme_punctuations, _phonemes, _arpabet, phonemes, symbols
 
 if __name__ == '__main__':
     print(" > TTS symbols ")
