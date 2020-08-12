aa560bec463b31724342eca53ff8f9a71e6d18de
marzus555 <62560070+marzus555@users.noreply.github.com>
2020-08-12 17:35:52 +0300

    Update generic_utils.py

--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -109,7 +109,7 @@ def _trim_model_state_dict(state_dict):
     return new_state_dict
 
 
-def save_checkpoint(model, optimizer, optimizer_st, optimizer_rc, model_loss, out_path,
+def save_checkpoint(model, optimizer, optimizer_st, model_loss, out_path,
                     current_step, epoch):
     checkpoint_path = 'checkpoint_{}.pth.tar'.format(current_step)
     checkpoint_path = os.path.join(out_path, checkpoint_path)
